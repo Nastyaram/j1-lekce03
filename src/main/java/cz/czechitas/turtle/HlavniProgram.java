fbfa7aed00ba2e68d3fff88455fe9201659e56bb
nastyaram <a.ramodina@gmail.com>
2023-02-21 20:37:48 +0100

    ctverec barevny

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,36 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
-    private Turtle zofka;
+    private Turtle zofka = new Turtle();
 
     public void start() {
-        zofka = new Turtle();
+
+       ctverec(100, Color.GREEN);
+
+       zofka.penUp();
+       zofka.turnLeft(90);
+       zofka.move(300);
+       zofka.turnRight(90);
+       zofka.penDown();
+
+       ctverec(200, Color.MAGENTA);
+
+
+
     }
+
+    public void ctverec(double delkaStrany, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 4; i++) {
+         zofka.move(delkaStrany);
+         zofka.turnRight(90);
+        }
+    }
+
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
